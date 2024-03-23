1f21cfd86c63bbe6befe472d5fd893f038daef65
Liam <lw433891@gmail.com>
2024-03-23 20:37:18 +0300

    'done'

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 12;
+let num4 = 3
+let mod = num3 / num4
+
+let mySet = new Set([10, 5, 20, 15]);
+
+let max = Math.max(...mySet);
